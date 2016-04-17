44952fb26ba86467f81a7e97118889eece8dcab9
Naeem Shaikh <lion@Lions-Air.mshome.net>
2016-04-17 20:19:02 +0530

    Added Calculator Brain Model

---
+++ Calculator/CalculatorVC.swift
@@ -0,0 +1,63 @@
+//
+//  CalculatorVC.swift
+//  Calculator
+//
+//  Created by Naeem Shaikh on 24/03/16.
+//  Copyright © 2016 Naeem Shaikh. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
